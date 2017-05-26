af0e859e3d105f502f52b641974f095d47901772
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-26 12:29:33 +0100

    Strategy Pattern Tutorial

---
+++ 3.9. Strategy/Michael/StrategyPaternTutorial/StrategyPaternTutorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPaternTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWeapon weapon = null;
+            Character Red = new Character();
+
+            while (true)
+            {
+                Console.WriteLine("Choose a weapon for Red! (1 = Sword, 2 = Axe, 3 = Club)");
+                String input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        weapon = new Sword();
+                        break;
+                    case "2":
+                        weapon = new Axe();
+                        break;
+                    case "3":
+                        weapon = new Club();
+                        break;
+                    default:
+                        break;
+                }
+                Red.SetWeapon(weapon);
+                Red.Attack();
+            }
+        }
+    }
+}
---
+++ 3.9. Strategy/Michael/StrategyPaternTutorial/StrategyPaternTutorial/Weapons/Axe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPaternTutorial
+{
+    /// <summary>
+    /// Concrete Strategy Axe
+    /// </summary>
+    public class Axe : IWeapon
+    {
+        public void Use()
+        {
+            Console.WriteLine("Using Axe");
+        }
+    }
+}

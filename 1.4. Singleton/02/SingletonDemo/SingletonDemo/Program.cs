be8a42fcc01e70ade362c54b2d8a08c106606045
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-26 09:52:45 +0100

    02

---
+++ 1.4. Singleton/02/SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("This is the first message");
+
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("This is the second message");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1.4. Singleton/02/SingletonDemo/SingletonDemo/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+
+        private static Singleton _instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Singleton();
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value: " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}

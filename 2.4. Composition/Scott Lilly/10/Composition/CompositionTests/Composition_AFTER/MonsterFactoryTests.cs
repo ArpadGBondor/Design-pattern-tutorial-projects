5b9097af4fc27920304e6ee3feaaee8b10d79828
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-06-02 21:30:26 +0100

    Composite

---
+++ 2.4. Composition/Scott Lilly/10/Composition/CompositionTests/Composition_AFTER/MonsterFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Composition.Composition_AFTER;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition.Composition_AFTER.Tests
+{
+    [TestClass()]
+    public class MonsterFactoryTests
+    {
+        [TestMethod()]
+        public void Test_BitingMonster()
+        {
+            Monster crocodile = MonsterFactory.CreateMonster(MonsterFactory.MonsterType.Crocodile);
+
+            Assert.IsTrue(crocodile.CanBite);
+            Assert.IsFalse(crocodile.CanKick);
+            Assert.IsFalse(crocodile.CanPunch);
+            Assert.IsFalse(crocodile.CanSpit);
+        }
+
+        [TestMethod()]
+        public void Test_BitingKickingMonster()
+        {
+            Monster horse = MonsterFactory.CreateMonster(MonsterFactory.MonsterType.Horse);
+
+            Assert.IsTrue(horse.CanBite);
+            Assert.IsTrue(horse.CanKick);
+            Assert.IsFalse(horse.CanPunch);
+            Assert.IsFalse(horse.CanSpit);
+        }
+    }
+}
---
+++ 2.4. Composition/Scott Lilly/10/Composition/CompositionTests/Inheritance_AFTER/MonsterFactoryTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Composition.Inheritance_AFTER;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition.Inheritance_AFTER.Tests
+{
+    [TestClass()]
+    public class MonsterFactoryTests
+    {
+        [TestMethod()]
+        public void Test_BitingMonster()
+        {
+            Monster crocodile = MonsterFactory.CreateMonster(MonsterFactory.MonsterType.Crocodile);
+
+            Assert.IsTrue(crocodile is BitingMonster);
+        }
+
+        [TestMethod()]
+        public void Test_BitingKickingMonster()
+        {
+            Monster horse = MonsterFactory.CreateMonster(MonsterFactory.MonsterType.Horse);
+
+            Assert.IsTrue(horse is KickingMonster);
+
+            // This test will fail, because we cannot inherit from multiple base classes.
+            Assert.IsTrue(horse is BitingMonster);
+        }
+
+    }
+}
